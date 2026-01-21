0b6cf6200199ac90d9adf648ae6e34a3964eea05
codexuttam <uttamrajsingh423@gmail.com>
2026-01-21 15:53:27 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import * as Three from "three";
+
+const scene = new Three.Scene();
+const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.add(camera);
+
+const cubeGeometry = new Three.BoxGeometry(1, 1, 1);
+const cubeMaterial = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Three.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
